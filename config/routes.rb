92e632da0f620e32af0ed62d11ba00d8739be839
Thibault Pedelhez <thi.pedelhez@gmail.com>
2017-04-08 00:59:50 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :opinions
-  resources :cards do
-    resources :opinions
+  resources :cards,  only: [:show] do
+    resources :opinions, only: [:create, :show, :update, :index, :new]
   end
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
